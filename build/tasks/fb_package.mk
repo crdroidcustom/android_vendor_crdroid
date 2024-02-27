a248affcdac9623644c0d5a25d39f33ccfc0f2ad
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2024-02-27 13:46:42 +0300

    vendor: Add fastboot packages build
    Change-Id: Ia6feb4eb842a9dd6b9b27bdd2f90c34b63dd22bb
    Signed-off-by: asalde_le1 <57564314+asaldele1@users.noreply.github.com>

---
+++ build/tasks/fb_package.mk
@@ -0,0 +1,16 @@
+LINEAGE_TARGET_FB_PACKAGE := $(PRODUCT_OUT)/crDroid-CUSTOM-v$(CR_VERSION)-FASTBOOT-$(CUSTOM_EDITION)-$(CUSTOM_BUILD_DATE).zip
+
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+
+.PHONY: fb_package
+fb_package: $(BUILT_TARGET_FILES_PACKAGE) $(IMG_FROM_TARGET_FILES_EXTENDED)
+	$(call pretty,"Package fastboot: $(LINEAGE_TARGET_FB_PACKAGE)")
+	PATH=$(INTERNAL_USERIMAGES_BINARY_PATHS):$(dir $(ZIP2ZIP)):$$PATH \
+	    $(IMG_FROM_TARGET_FILES_EXTENDED) \
+	        --additional_zip $(PRODUCT_FASTBOOT_TEMPLATE_ZIP) \
+	        --exclude_android_info \
+	        --images_path $(PRODUCT_FASTBOOT_IMAGES_PATH) \
+	        $(BUILT_TARGET_FILES_PACKAGE) $(LINEAGE_TARGET_FB_PACKAGE)
+	$(hide) $(SHA256) $(LINEAGE_TARGET_FB_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_FB_PACKAGE).sha256sum
+	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
+	@echo "Package Complete: $(LINEAGE_TARGET_FB_PACKAGE)" >&2
