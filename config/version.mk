a6b621b3ba5b904920a127ac0e05ebc34e02b4e9
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2024-02-27 13:46:42 +0300

    vendor: Add fastboot packages build
    Change-Id: Ia6feb4eb842a9dd6b9b27bdd2f90c34b63dd22bb
    Signed-off-by: asalde_le1 <57564314+asaldele1@users.noreply.github.com>

--- config/version.mk
+++ config/version.mk
@@ -1,14 +1,23 @@
 PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
+ifeq ($(WITH_GMS),true)
+CUSTOM_EDITION := GAPPS
+else
+CUSTOM_EDITION := VANILLA
+endif
+
+CUSTOM_BUILD_DATETIME := $(shell date +%s)
+CUSTOM_BUILD_DATE := $(shell date -d @$(CUSTOM_BUILD_DATETIME) +"%Y%m%d-%H%M%S")
+
 # Increase CR Version with each major release.
 CR_VERSION := 11.2
 
 # Internal version
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
+LINEAGE_VERSION := crDroid-CUSTOM-v$(CR_VERSION)-$(CUSTOM_EDITION)-$(CUSTOM_BUILD_DATE)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := v$(CR_VERSION)-$(shell date +%Y%m%d)
+LINEAGE_DISPLAY_VERSION := v$(CR_VERSION)-$(CUSTOM_EDITION)-$(CUSTOM_BUILD_DATE)
 
 # LineageOS version properties
 PRODUCT_SYSTEM_PROPERTIES += \
