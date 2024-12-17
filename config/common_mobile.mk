97c41bdbe54f11ed27f30c5bf970f7fe936c60f5
priiii08918 <priyanshusinghal0818@proton.me>
2024-12-17 10:52:03 +0000

    vendor: Bringup custom crDroid
    Change-Id: I634a1fd2597cec7e6d99b97bf7df778caead7840

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,33 +1,15 @@
 # Inherit common mobile Lineage stuff
 $(call inherit-product, vendor/lineage/config/common.mk)
 
-# Include AOSP audio files
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-include vendor/lineage/config/aosp_audio.mk
-
-# Include Lineage audio files
-include vendor/lineage/config/lineage_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
-
 # Apps
 PRODUCT_PACKAGES += \
-    AvatarPicker \
     Backgrounds \
-    Glimpse \
     LatinIME
 
-# Charger
+ifneq ($(WITH_GMS),true)
 PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
+    AvatarPicker \
+    Glimpse
 endif
 
 # Legal
